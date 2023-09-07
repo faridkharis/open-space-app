bc903567632b16a77be0efb5f89ca240049bf4c7
Muhamad Farid Kharismawan <faridkharismawan@gmail.com>
2023-09-07 10:59:08 +0700

    fix: add parameter onAddTalk and onLike

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,40 +1,40 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import TalkInput from '../components/TalkInput';
 import TalksList from '../components/TalksList';
 import { asyncPopulateUsersAndTalks } from '../states/shared/action';
 import { asyncAddTalk, asyncToggleLikeTalk } from '../states/talks/action';
-
+ 
 function HomePage() {
   const {
     talks = [],
     users = [],
     authUser,
   } = useSelector((states) => states); // @TODO: get talks, users, and authUser state from store
-
+ 
   const dispatch = useDispatch(); // @TODO: get dispatch function from store
-
+ 
   useEffect(() => {
     // @TODO: dispatch async action to populate talks and users data
     dispatch(asyncPopulateUsersAndTalks());
   }, [dispatch]);
-
+ 
   const onAddTalk = (text) => {
     // @TODO: dispatch async action to add talk
-    dispatch(asyncAddTalk())
+    dispatch(asyncAddTalk({ text }));
   };
-
+ 
   const onLike = (id) => {
     // @TODO: dispatch async action to toggle like talk
-    dispatch(asyncToggleLikeTalk())
+    dispatch(asyncToggleLikeTalk(id));
   };
-
+ 
   const talkList = talks.map((talk) => ({
     ...talk,
     user: users.find((user) => user.id === talk.user),
     authUser: authUser.id,
   }));
-
+ 
   return (
     <section className="home-page">
       <TalkInput addTalk={onAddTalk} />
@@ -42,5 +42,5 @@ function HomePage() {
     </section>
   );
 }
-
+ 
 export default HomePage;
